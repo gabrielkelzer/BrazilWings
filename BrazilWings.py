c576d962bd271a2c0118461faa6710e861c35641
MARKTING DE SOFTWARE <64110393+gabrielkelzer@users.noreply.github.com>
2024-02-06 19:31:00 -0300

    Add files via upload

---
+++ BrazilWings.py
@@ -0,0 +1,174 @@
+import os
+clear = lambda: os.system('clear')
+#maioria dos softwares estão na pasta principal, ou seja.... algumas funções do menu estão neste arquivo
+iniciar_helewings = lambda: os.system('cd;cd HeleWings;. ./HeleWings.sh;cd;cd BrazilWings')
+iniciar_TentameKelzer = lambda: os.system('cd;cd Tentame_Kelzer;. ./TentameKelzer.sh;cd;cd BrazilWings')
+iniciar_SYSTEMNERF = lambda: os.system('cd;cd SYSTEM_NERF;. ./systemnerf.sh;cd;cd BrazilWing')
+copiar_app_virusapk = lambda: os.system('cp -i Virus.apk /data/data/com.termux/files/home/storage/downloads')
+copiar_app_smsbomber = lambda: os.system(' cp -i Smsbomber.apk /data/data/com.termux/files/home/storage/downloads')
+abrir_site_livro = lambda: os.system('termux-open-url https://clubedeautores.com.br/livro/achado-nao-e-roubado-2')
+sleep = lambda: os.system('sleep 3')
+umarquivoemshell = lambda: os.system('. ./baixarresto.sh')
+link_manual = lambda: os.system('termux-open-url https://zonamestre.blogspot.com/2024/02/software-para-hacking-e-pentest-do.html')
+iniciar_kelzerscan = lambda: os.system('cd;cd KelzerScan;python KelzerScan.py;cd;cd BrazilWings')
+iniciar_kelzerstar = lambda: os.system('cd;cd KelzerStar;python KelzerStar.py;cd;cd BrazilWings')
+
+
+
+
+while MENU_CONFG != "1":
+     clear()
+     print ('''
+          \033[36m--------------------\033[0m
+         \033[33m SEJA BEM-VINDO(A) :D\033[0m
+        \033[36m  --------------------\033[0m
+        \033[31m  MENU DE CONFIGURAÇÕES INICIAIS:\033[0m
+        \033[34m  1 - INICIAR
+          2 - BAIXAR O RESTANTE DO SCRIPT
+          3 - MANUAL COMPLETO \033[0m
+''')
+     MENU_CONFG = input('\033[33mDIGITE UM NÚMERO:\033[0m')
+     clear()
+
+
+
+     # condicional encadeado
+     if nota == 1:
+          print('\033[31mOK\033[0m')
+          sleep()
+          print ('\033[31mINICIANDO...\033[0m')
+          sleep()
+     elif nota == 2:
+          umarquivoemshell()
+     elif nota == 3:
+          link_manual()
+     else:
+          print('\033[31mVOCÊ DIGITOU UM NÚMERO DIFERENTE ! ;-; \033[0m')
+          sleep()
+
+
+
+while MENU_BW != "9":
+     clear()
+     print ('''
+l=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=l
+l                  LICENÇA GPL                    l
+l=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=l
+l 卐           ─▄────▄▄▄▄▄▄▄────▄─             卐 l
+l              ▀▀▄─▄█████████▄─▄▀▀                l
+l              ────██─▀███▀─██────                l
+l              ──▄─▀████▀████▀─▄──                l
+l              ▀█────██▀█▀██────█▀                l
+l                                                 l
+l                 BrazilWings V1                  l
+l         /-----------------------------\         l
+l         l [1] PortScan em Python      l         l
+l         l                             l         l
+l         l [2] BruteForce para achar   l         l
+l         l     pastas e arquivos de    l         l
+l         l     sites.                  l         l
+l         l                             l         l
+l         l [3] Menu de instalações e   l         l
+l         l     de personalizar o       l         l
+l         l     o Termux                l         l
+l         l                             l         l
+l         l [4] Enviar aúdio falso no   l         l
+l         l     WhatsApp.               l         l
+l         l                             l         l
+l         l [5] Sistema antitravas Zap  l         l
+l         l     (ROOT)                  l         l
+l         l                             l         l
+l         l [6] Obter Apks de Hacking   l         l                        
+l         l                             l         l
+l         l [7] Código de cor para      l         l
+l         l     criar meus próprios     l         l
+l         l     Scripts.                l         l
+l         l                             l         l
+l         l [8] O que é licença GPL ?   l         l
+l         l                             l         l
+l         l [9] Comprar o livro do      l         l
+l         l      desenvolvedor          l         l
+l         l                             l         l
+l         l [10] Sair                   l         l
+l         l                             l         l
+l         \-----------------------------/         l
+l         By: Gabriel Kelzer(@bielkelzer)         l
+l 卐                ┌∩┐(◣_◢)┌∩┐                卐 l
+---------------------------------------------------
+
+''')      
+     MENU_BW = input('\033[33mDIGITE UM NÚMERO:\033[0m')
+     print('\033[1;34mSABIA QUE A COMUNIDADE KELZER COMUNITY CHAMA O BRAZIL WINGS DE LAZYMUX BRASILEIRO ?\033[m')
+     sleep()
+     clear()
+
+
+     match MENU_BW:  
+          case 1:
+               iniciar_kelzerscan()
+          case 2:
+               iniciar_kelzerstar()
+          case 3:
+               iniciar_helewings()
+          case 4:
+               iniciar_TentameKelzer()
+          case 5:
+               iniciar_SYSTEMNERF()
+          case 6:
+               copiar_app_virusapk()
+               copiar_app_smsbomber()
+               clear()
+               print ('''\033[31mPRONTO, AGORA VERIFIQUE A SUA PASTA DE DOWNLOADS.\033[0m:
+                      ''')
+               OP_PAUSAR = input('\033[33mAPERTE ENTER PARA VOLTAR AO MENU ANTERIOR:\033[0m')
+          case 7:
+               print('\033[1m - NEGRITO\033[m \ 0 3 3 [ 1 m')
+               print('\033[3m - ITÁLICO\033[m \ 0 3 3 [ 3 m')
+               print('\033[4m - SUBLINHADO\033[m \ 0 3 3 [ 4 m')
+               print('\033[7m - COR INVERTIDA\033[m \ 0 3 3 [ 7 m')
+               print('\033[9m - TACHADO\033[m \ 0 3 3 [ 9 m')
+               print('\033[21m - SUBLINHADO\033[m \ 0 3 3 [ 2 1 m')
+               print('\033[30m - TEXTO PRETO\033[m \ 0 3 3 [ 3 0 ')
+               print('\033[31m - TEXTO VERMELHO\033[m \ 0 3 3 [ 3 1 m')
+               print('\033[32m - TEXTO VERDE\033[m \ 0 3 3 [ 3 2 m')
+               print('\033[33m - TEXTO AMARELO\033[m \ 0 3 3 [ 3 3 m')
+               print('\033[34m - TEXTO AZUL\033[m \ 0 3 3 [ 3 4 m')
+               print('\033[35m - TEXTO MAGENTA\033[m \ 0 3 3 [ 3 5 m')
+               print('\033[36m - TEXTO CIANO\033[m \ 0 3 3 [ 3 6 m')
+               print('\033[37m - TEXTO CINZA\033[m \ 0 3 3 [ 3 7 m')
+               print('\033[40m - FUNDO PRETO\033[m \ 0 3 3 [ 4 0 m')
+               print('\033[41m - FUNDO VERMELHO\033[m \ 0 3 3 [ 4 1 m')
+               print('\033[42m - FUNDO VERDE\033[m \ 0 3 3 [ 4 2 m')
+               print('\033[43m - FUNDO AMARELO\033[m \ 0 3 3 [ 4 3 m')
+               print('\033[44m - FUNDO AZUL\033[m \ 0 3 3 [ 4 4 m')
+               print('\033[45m - FUNDO MAGENTA\033[m \ 0 3 3 [ 4 5 m')
+               print('\033[46m - FUNDO CIANO\033[m \ 0 3 3 [ 4 6 m')
+               print('\033[47m - FUNDO CINZA\033[m \ 0 3 3 [ 4 7 m')
+               print('\033[51m - TEXTO COM BORDA\033[m \ 0 3 3 [ 5 1 m')
+               print('\033[52m - TEXTO COM BORDA\033[m \ 0 3 3 [ 5 2 m')
+               print('\033[90m - TEXTO AZUL ESCURO\033[m \ 0 3 3 [ 9 0 m')
+               print('\033[100m - FUNDO AZUL CLARO\033[m \ 0 3 3 [ 1 0 0 m')
+               OP_PAUSAR = input('\033[33mAPERTE ENTER PARA VOLTAR AO MENU ANTERIOR:\033[0m')
+          case 8:
+               print ('''\033[34m
+Este código tem licença GPL, isso quer dizer que os usuários podem modificar o script e trocar o nome do criador. Porém, eles precisam dizer em alguma parte do programa a seguinte frase "Este Software foi baseado no código-fonte do script BrazilWings.
+                    \033[0m  ''')
+               OP_PAUSAR = input('\033[33mAPERTE ENTER PARA VOLTAR AO MENU ANTERIOR:\033[0m')
+          case 9:
+               abrir_site_livro()
+          case 10:
+               print ('''
+\033[1;32mDESENVOLVEDOR:\033[0m
+     \033[1;34mGabriel Kelzer.\033[0m
+\033[1;32mINSTAGRAM:\033[0m
+     \033[1;34m@bielkelzer.\033[0m
+\033[1;32mCOMUNIDADE ADMINISTRADORA DA SCRIPT:\033[0m
+     \033[1;34mKelzer Comunity\033[0m\033[1;32m
+GRUPO DO TELEGRAM:\033[0m\033[1;34m
+     HTTPS://T.ME/KELZERCOMUNITY\033[0m
+\033[03;32m"As vezes criatividade é continuar criando coisas mesmo não sendo reconhecido", Gabriel Kelzer.\033[0m
+''')
+          case _:
+               print ('''\033[31mO número que você digitou é inválido !!!\033[0m
+                      ''')
+               OP_PAUSAR = input('\033[33mAPERTE ENTER PARA VOLTAR AO MENU ANTERIOR:\033[0m')
