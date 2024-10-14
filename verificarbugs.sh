b3d13a3bc09f31afe563e2f345558c8e7094f9da
Gabriel Cardoso <64110393+gabrielkelzer@users.noreply.github.com>
2024-10-14 20:33:42 -0300

    Create verificarbugs.sh

---
+++ verificarbugs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo -e "VERIFICANDO SE É POSSÍVEL EXECUTAR O SOFTWARE NO SEU CELULAR..."
+sleep 3
+clear
+echo -e "VERIFICANDO O PROTOCOLO 7..."
+sleep 3
+clear
+cd
+rm -rf BUGS2024
+git clone https://github.com/gabrielkelzer/BUGS2024
+cd BUGS2024
+chmod +x *
+. ./PROTOCOLO7.sh
