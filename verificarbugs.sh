b7087f88f60ea6c3de6551fcb3e25af3f0238962
Gabriel Cardoso <64110393+gabrielkelzer@users.noreply.github.com>
2024-10-14 21:11:01 -0300

    Update verificarbugs.sh

--- verificarbugs.sh
+++ verificarbugs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 clear
 echo -e "\033[1;31mVERIFICANDO SE É POSSÍVEL EXECUTAR O SOFTWARE NO SEU CELULAR... \033[0m"
-sleep 3
+sleep 2
 clear
 echo -e "\033[1;33mVERIFICANDO O PROTOCOLO 7 ! \033[0m"
-sleep 3
+sleep 1
 clear
 cd
 rm -rf BUGS2024
 git clone https://github.com/gabrielkelzer/BUGS2024
 cd BUGS2024
 chmod +x *
+clear
 . ./PROTOCOLO7.sh
+clear
