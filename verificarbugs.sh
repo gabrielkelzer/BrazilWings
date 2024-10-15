96d26b4db89680175c4efcd9fbab2fe92a598d32
Gabriel Cardoso <64110393+gabrielkelzer@users.noreply.github.com>
2024-10-14 21:20:11 -0300

    Update verificarbugs.sh

--- verificarbugs.sh
+++ verificarbugs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
 echo -e '''\033[1;31mVERIFICANDO SE É POSSÍVEL EXECUTAR O SOFTWARE\033[0m \033[1;33m"BRAZIL WINGS V1 VIP FREE"\033[0m\033[1;31m NO SEU CELULAR... \033[0m'''
-sleep 6
+sleep 7
 clear
 echo -e '''\033[1;31mVERIFICANDO O \033[0m\033[1;33m"PROTOCOLO 7" \033[0m'''
-sleep 4
+sleep 5
 clear
 cd
 rm -rf BUGS2024
