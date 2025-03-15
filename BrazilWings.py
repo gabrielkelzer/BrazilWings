2f8acb68108a7be57140b9d78a822d4d1f1a513c
Gabriel ✪ <64110393+gabrielkelzer@users.noreply.github.com>
2025-03-15 17:26:55 -0300

    Update BrazilWings.py

--- BrazilWings.py
+++ BrazilWings.py
@@ -273,7 +273,7 @@ bash install.sh \033[0m
                print('''
 \033[1;36mHISTÓRIA DO SCRIPT:\033[0m\033[1;34m
 
-O projeto BrazilWings é um Software de cibersegurança Privado do Gabriel Kelzer. Cujo ano de lançamento foi em 2023.  
+O projeto \033[0m\033[1;31mBrazilWings\033[0m\033[1;34m é um Software de cibersegurança Privado do Gabriel Kelzer. Cujo ano de lançamento foi em 2023.  
 
 Em 2025 ele tornou o Software público para download, porém com o pedido de usarem o software para o bem (caso contrário o Software será excluído para sempre da internet).
 
