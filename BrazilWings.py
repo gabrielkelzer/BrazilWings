a9385384c0d589f74dd7e038792c6ee2a812d3e1
Gabriel ✪ <64110393+gabrielkelzer@users.noreply.github.com>
2025-03-15 16:57:24 -0300

    Update BrazilWings.py

--- BrazilWings.py
+++ BrazilWings.py
@@ -272,41 +272,17 @@ bash install.sh \033[0m
           case "11":
                print('''
 \033[1;32mHISTÓRIA DO SCRIPT:\033[0m\033[1;31m
+O projeto BrazilWings é um Software de cibersegurança Privado do Gabriel Kelzer. Cujo ano de lançamento foi em 2023.  
 
-O projeto BrazilWings é um Software de cibersegurança Privado do Gabriel Kelzer. Cujo ano de lançamento foi em 2023. 
+Em 2025 ele tornou o Software pública para download, porém com o pedido de usarem o software para o bem (caso contrário o Software será excluído para sempre da internet).
 
 Por possuir todas as ferramentas de cibersegurança criadas por ele... então este Software acabou se tornando uma das melhores opções de Pentest no Android.
-
-VERSÃO V1 VIP FREE :
 
 Infelizmente o Software continua rodando somente no Termux.
 
 O único problema que os usuários devem tomar cuidado é que eles não podem instalar o APK "VIRUS.APK" pois ele Formata o celular da pessoa que instalar.
 
-Também vale destacar que o script tá todo automatizado, Ou seja. Após o usuário instalar o Script vai dar um menu de configuração iniciais. Portanto basta digitar "python brazilwings.py" E SER FELIZ :D
-
-FUNÇÕES DA FERRAMENTA:
-
-• Menu de instalações;
-• Abrir/mostra sites úteis caso queira invadir sites;
-• Enviar áudio falso no WhatsApp;
-• Subir/bugar as travas do WhatsApp;
-• PortScan avançado(criado pelo próprio criador da BrazilWing);
-• Personalizar o Termux;
-• Obter Apk que formatar o celular da pessoa;
-• Sms bomber;
-• Te ajuda a programar te dando uma lista de cores em código;
-• Personaliza o Termux;
-• Te ensina o básico de hacking e Pentest(tutoriais e etc..)
-• Te ensina a programar introduzindo á lógica de programação;
-• Lista estados pelo número de Telefone;
-• Ele possui um livro Hacker criado pelo próprio Desenvolvedor do Software;
-
-COMANDOS:
-
-Este texto pode se tornar obsolento com o tempo, portanto acesse:
-
-Zonamestre.blogspot.com 
+Também vale destacar que o script tá todo automatizado, Ou seja. Após o usuário instalar o Script vai dar um menu de configuração iniciais. Portanto basta digitar "python brazilwings.py" E SER FELIZ :D 
 
 Ass: Gabriel Kelzer\033[0m
 ''')
